8e6fccb321ae3313815423e6794b1f38f34974da
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-08-16 21:26:00 +0300

    Добавьте файлы проекта.

---
+++ NetUserTypesApp/Examples.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetUserTypesApp
+{
+    static class Examples
+    {
+        public static void NameSpaces()
+        {
+            //using MyName;
+
+            //Console.WriteLine("Hello");
+            //Person person;
+
+            //namespace MyName
+            //{
+            //    class Person { }
+            //}
+        }
+    }
+}
---
+++ NetUserTypesApp/Program.cs
@@ -0,0 +1,31 @@
+﻿Employee employee = new Employee();
+
+
+employee.name = "Bobby";
+employee.age = 29;
+
+employee.Print();
+
+
+var employee1 = new Employee();
+Employee employee2 = new();
+
+
+class Employee
+{
+    public string name;
+    public int age;
+
+    public void Print()
+    {
+        Console.WriteLine("Name: " + name + ", Age: " + age);
+    }
+
+    public Employee() { }
+
+    public Employee(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+}
