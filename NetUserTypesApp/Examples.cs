4e419839deb9fb1362b91d84e0cd2a7bf6116b1f
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-08-19 19:47:07 +0300

    19-08-24

--- NetUserTypesApp/Examples.cs
+++ NetUserTypesApp/Examples.cs
@@ -20,5 +20,35 @@ namespace NetUserTypesApp
             //    class Person { }
             //}
         }
+
+        public static void InterpolatonString()
+        {
+            string city = "Moscow";
+            int code = 77;
+
+            Console.WriteLine($"{city:#-#-#-#-#}");
+
+            Console.WriteLine($"City name: {city}, code: {code}, 2 + 2 = {Sum(2, 2)}");
+            Console.WriteLine($"City name: {1}, code: {0}, 2 + 2 = {2}", code, city, Sum(2, 2));
+
+            decimal price = 100.2M;
+            string sprice = string.Format("{0:C2}", price);
+            Console.WriteLine($"{price:C2}");
+
+            int num = 15;
+            Console.WriteLine($"{num:D4}");
+
+            double x = 0.5;
+            Console.WriteLine($"{x:F1}");
+            Console.WriteLine($"{x:P}");
+
+            long phone = 79991234567;
+            Console.WriteLine($"{phone:+# (###) ###-##-##}");
+
+            int Sum(int a, int b)
+            {
+                return a + b;
+            }
+        }
     }
 }
--- NetUserTypesApp/Program.cs
+++ NetUserTypesApp/Program.cs
@@ -1,19 +1,46 @@
-﻿Employee employee = new Employee();
+﻿using NetUserTypesApp;
+using System.ComponentModel;
 
+Employee employee = new Employee();
 
 employee.name = "Bobby";
 employee.age = 29;
 
+
 employee.Print();
 
+Employee bob = new Employee("Bobby", 26);
 
-var employee1 = new Employee();
-Employee employee2 = new();
+(string n, int a) = bob;
+Console.WriteLine(n);
+Console.WriteLine(a);
+
+ClPerson cperson1 = new() { name = "Bob", age = 27 };
+ClPerson cperson2 = cperson1;
+cperson2.name = "Leo";
+Console.WriteLine(cperson1.name);
+
+StPerson sperson1 = new() { name = "Bob", age = 27 };
+StPerson sperson2 = sperson1;
+sperson2.name = "Leo";
+Console.WriteLine(sperson1.name);
+
+
+RcPerson rperson1 = new() { name = "Bob", age = 27 };
+RcPerson rperson2 = rperson1;
+rperson2.name = "Leo";
+Console.WriteLine(rperson1.name);
+
+void Functuion()
+{
+    i = 10;
+}
 
 
 class Employee
 {
     public string name;
+    public string address;
     public int age;
 
     public void Print()
@@ -28,4 +55,32 @@ class Employee
         this.name = name;
         this.age = age;
     }
+
+    public Employee(string name) : this(name, 0) { }
+    public Employee(int age) : this("Anonim", age) { }
+
+    public void Deconstruct(out string name, out int age)
+    {
+        name = this.name;
+        age = this.age;
+    }
+}
+
+class ClPerson
+{
+    public string name;
+    public int age;
+}
+
+struct StPerson
+{
+    public string name;
+    public int age;
+    public ClPerson parent;
+}
+
+record RcPerson
+{
+    public string name;
+    public int age;
 }
